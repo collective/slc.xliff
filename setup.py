c3101316bcd398094e7d5a79e27c32093e34fcd4
csenger <csenger@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-09-11 08:28:51 +0000

    change version to 1.2-dev

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.1'
+version = '1.2-dev'
 
 long_description = (
     read('README.txt')

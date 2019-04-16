aac3d25a2c90642bb2d5e8fc633605dad8cb15e8
Mikel Larreategi <mlarreategi@codesyntax.com>
2019-04-16 17:00:23 +0200

    move doctests to standard tests

--- src/slc/xliff/tests/base.py
+++ src/slc/xliff/tests/base.py
@@ -1,49 +1,66 @@
+from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
+from plone.app.multilingual.testing import MultipleLanguagesLayer
+from plone.app.multilingual.testing import PLONE_APP_MULTILINGUAL_PRESET_FIXTURE
+from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
-from plone.app.testing import PLONE_FIXTURE
-from plone.app.testing import applyProfile
+from plone.app.testing import login
+from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import setRoles
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import TEST_USER_NAME
-from plone.app.testing import login
 from plone.testing import z2
 from Products.Five import fiveconfigure
+from Products.CMFPlone.utils import getToolByName
 
-from plone.app.multilingual.testing import PloneAppMultilingualLayer
 
-class SlcXliffLayer(PloneAppMultilingualLayer):
-    defaultBases = (PLONE_FIXTURE, )
-        
+class SlcXliffLayer(PloneSandboxLayer):
+    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
+
     def setUpZope(self, app, configurationContext):
-        PloneAppMultilingualLayer.setUpZope(self, app, configurationContext)
-        z2.installProduct(app, 'Products.PloneLanguageTool')
-        z2.installProduct(app, 'Products.ZCatalog')
+        super(SlcXliffLayer, self).setUpZope(app, configurationContext)
+        # z2.installProduct(app, "Products.ZCatalog")
 
         fiveconfigure.debug_mode = True
+        import plone.app.multilingual
+
+        self.loadZCML("configure.zcml", package=plone.app.multilingual)
+
         import slc.xliff
-        self.loadZCML('configure.zcml', package=slc.xliff)
-        self.loadZCML('testing.zcml', package=slc.xliff)
+
+        self.loadZCML("configure.zcml", package=slc.xliff)
+        self.loadZCML("testing.zcml", package=slc.xliff)
+
         fiveconfigure.debug_mode = False
+        z2.installProduct(app, "slc.xliff")
 
     def setUpPloneSite(self, portal):
-        PloneAppMultilingualLayer.setUpPloneSite(self, portal)
+        portal.acl_users.userFolderAddUser(
+            SITE_OWNER_NAME, SITE_OWNER_PASSWORD, ["Manager"], []
+        )
+        login(portal, SITE_OWNER_NAME)
+        setRoles(portal, TEST_USER_ID, ["Manager"])
 
-        applyProfile(portal, 'slc.xliff:default')
-        applyProfile(portal, 'slc.xliff:testing')
+        language_tool = getToolByName(portal, "portal_languages")
+        language_tool.addSupportedLanguage("en")
+        language_tool.addSupportedLanguage("de")
+        language_tool.addSupportedLanguage("es")
+        language_tool.addSupportedLanguage("fi")
 
-        # Login as manager and create a test folder
-        setRoles(portal, TEST_USER_ID, ['Manager'])
-        login(portal, TEST_USER_NAME)
-        portal.invokeFactory('Folder', 'folder')
+        applyProfile(portal, "plone.app.multilingual:default")
+        applyProfile(portal, "slc.xliff:default")
+        applyProfile(portal, "slc.xliff:testing")
 
         # Commit so that the test browser sees these objects
-        portal.portal_catalog.clearFindAndRebuild()
-        import transaction
-        transaction.commit()
+        # portal.portal_catalog.clearFindAndRebuild()
+        # import transaction
 
-    def tearDownZope(self, app):
-        z2.uninstallProduct(app, 'Products.PloneLanguageTool')
-        z2.uninstallProduct(app, 'Products.ZCatalog')
+        # transaction.commit()
+
+    # def tearDownZope(self, app):
+    #     z2.uninstallProduct(app, "Products.ZCatalog")
 
 
 SLC_XLIFF_FIXTURE = SlcXliffLayer()
---
+++ src/slc/xliff/tests/test_setup.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""Setup tests for this package."""
+from slc.xliff.tests.base import INTEGRATION_TESTING  # noqa: E501
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+
+import unittest
+
+try:
+    from Products.CMFPlone.utils import get_installer
+except ImportError:
+    get_installer = None
+
+
+class TestSetup(unittest.TestCase):
+    """Test that slc.xliff is properly installed."""
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer["portal"]
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer["request"])
+        else:
+            self.installer = api.portal.get_tool("portal_quickinstaller")
+
+    def test_product_installed(self):
+        """Test if slc.xliff is installed."""
+        self.assertTrue(self.installer.isProductInstalled("slc.xliff"))
+
+    def test_browserlayer(self):
+        """Test that ISlcXliff is registered."""
+        from slc.xliff.interfaces import ISlcXliff
+        from plone.browserlayer import utils
+
+        self.assertIn(ISlcXliff, utils.registered_layers())
+
+
+class TestUninstall(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer["request"])
+        else:
+            self.installer = api.portal.get_tool("portal_quickinstaller")
+        roles_before = api.user.get_roles(TEST_USER_ID)
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+        self.installer.uninstallProducts(["slc.xliff"])
+        setRoles(self.portal, TEST_USER_ID, roles_before)
+
+    def test_product_uninstalled(self):
+        """Test if slc.xliff is cleanly uninstalled."""
+        self.assertFalse(self.installer.isProductInstalled("slc.xliff"))
+
+    def test_browserlayer_removed(self):
+        """Test that ISlcXliff is removed."""
+        from slc.xliff.interfaces import ISlcXliff
+        from plone.browserlayer import utils
+
+        self.assertNotIn(ISlcXliff, utils.registered_layers())

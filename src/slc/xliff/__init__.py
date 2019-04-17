622c7b5379c740dad7eca50e3688144db3998d79
Mikel Larreategi <mlarreategi@codesyntax.com>
2019-04-17 08:41:55 +0200

    we do not need to register the package

--- src/slc/xliff/__init__.py
+++ src/slc/xliff/__init__.py
@@ -1,8 +1,3 @@
 from zope.i18nmessageid import MessageFactory
 
 XliffMessageFactory = MessageFactory('slc.xliff')
-
-
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""

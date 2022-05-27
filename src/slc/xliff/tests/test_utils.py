d804b463b68dbc2049a1fb18971d39b40f65d876
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-05-27 09:44:31 +0200

    one more test

--- src/slc/xliff/tests/test_utils.py
+++ src/slc/xliff/tests/test_utils.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
+
+from plone import api
 from slc.xliff.tests.base import INTEGRATION_TESTING
 from slc.xliff.xliff import _guessLanguage
 
@@ -40,4 +42,15 @@ class TestUtils(unittest.TestCase):
     def test_guessLanguage_without_language(self):
         """test that a filename without language code returns an empty string"""
         filename = "filename_mixed.xliff"
+        self.assertEqual(_guessLanguage(filename), "")
+
+    def test_unexisting_language(self):
+        """test that a filename with an unexisting language is ignored"""
+        inexisting_language = "fr"
+        filename = "filename_{0}.xliff".format(inexisting_language)
+        supported_languages = api.portal.get_tool(
+            "portal_languages"
+        ).getSupportedLanguages()
+        self.assertNotIn(inexisting_language, supported_languages)
+
         self.assertEqual(_guessLanguage(filename), "")
